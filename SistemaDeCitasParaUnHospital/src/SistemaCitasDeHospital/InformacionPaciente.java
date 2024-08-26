e200e8956d3c7d2affadab77df0b0dca6b96dc27
unknown <jaimeulloayero15@gmail.com>
2024-08-26 23:04:15 +0200

    creamos los get de la clase

--- SistemaDeCitasParaUnHospital/src/SistemaCitasDeHospital/InformacionPaciente.java
+++ SistemaDeCitasParaUnHospital/src/SistemaCitasDeHospital/InformacionPaciente.java
@@ -1,15 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package SistemaCitasDeHospital;
-
-/**
- *
- * @author jaime
- */
 public class InformacionPaciente {
+    private String idDeCita;
+    private String nombreDelPaciente;
+    private String idDelPaciente;
+    private String especialidad;
+    private String fecha;
+    private String hora;
+    private boolean estado;
+    
+    public InformacionPaciente(String idDecita, String nombreDelPaciente,
+            String idDelPaciente, String especialidad, String fecha, String hora,
+            boolean estado){
+        this.especialidad=especialidad;
+        this.estado=estado;
+        this.fecha=fecha;
+        this.idDeCita=idDecita;
+        this.idDelPaciente=idDelPaciente;
+        this.nombreDelPaciente=nombreDelPaciente;
+        this.estado=false;
+    }
+    public String getIdDeCita(){
+        return idDeCita;
+    }
+    public String getNombreDelPaciente(){
+        return nombreDelPaciente;
+    }
+    public String getIdDelPaciente(){
+        return idDelPaciente;
+    }
+    public String getEspecialidad(){
+        return especialidad;
+    }
+    public String getFecha(){
+        return fecha;
+    }
+    public String getHora(){
+        return hora;
+    }
+    public boolean getEstado(){
+        return estado;
+    }
     
 }
