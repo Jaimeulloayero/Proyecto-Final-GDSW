6c8872d1694015ccce69c47593d78678af8e9ae0
unknown <jaimeulloayero15@gmail.com>
2024-08-29 09:33:05 +0200

    creamos una clase para paciente

---
+++ SistemaDeCitasParaUnHospital/src/SistemaCitasDeHospital/Paciente.java
@@ -0,0 +1,15 @@
+package SistemaCitasDeHospital;
+
+import java.util.List;
+
+public class Paciente {
+    private String idPaciente;
+    private String nombre;
+    private List<Cita> citas;
+    
+    public Paciente(String idPaciente, String nombre){
+        this.citas=citas;
+        this.idPaciente=idPaciente;
+        this.nombre=nombre;
+    }
+}
