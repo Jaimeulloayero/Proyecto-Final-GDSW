d49a58c747abc41cb302eeb0d21057903697639f
unknown <jaimeulloayero15@gmail.com>
2024-09-02 21:32:36 +0200

    agregamos pruebas unitarias al proyecto con errores aun

--- SistemaDeCitasParaUnHospital/src/SistemaCitasDeHospital/Paciente.java
+++ SistemaDeCitasParaUnHospital/src/SistemaCitasDeHospital/Paciente.java
@@ -6,16 +6,11 @@ import java.util.List;
 public class Paciente {
     private String idPaciente;
     private String nombre;
-    private List<Cita> citas;
- //   private List<Paciente> pacientes;
     
     public Paciente(String idPaciente, String nombre){
-        this.citas= new ArrayList<>();
         this.idPaciente=idPaciente;
         this.nombre=nombre;
-      //  this.pacientes= new ArrayList<>();
     }
-
     public String getIdPaciente() {
         return idPaciente;
     }
@@ -31,12 +26,4 @@ public class Paciente {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
-    public List<Cita> getCitas() {
-        return citas;
-    }
-
-    public void setCitas(List<Cita> citas) {
-        this.citas = citas;
-    }   
 }
